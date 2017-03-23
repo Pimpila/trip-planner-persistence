9f3755b4fa41587cf4de8f82545d68bf12bf4a64
Jacob Kayen <jacob.kayen@gmail.com>
2017-03-23 11:50:34 -0400

    api routes started

---
+++ routes/api.js
@@ -0,0 +1,71 @@
+var db = require('../models');
+var Hotel = db.model('hotel');
+var Restaurant = db.model('restaurant');
+var Activity = db.model('activity');
+var Day = db.model('day');
+var Day_Restaurant = db.model('day_restaurants');
+var router = require('express').Router();
+
+router.get('/hotels', function (req, res, next) {
+  Hotel.findAll()
+  .then(function (allHotels) {
+    res.json(allHotels)
+  })
+  .catch(next);
+});
+
+router.get('/restaurants', function (req, res, next) {
+  Restaurant.findAll()
+  .then(function (allRestaurants) {
+    res.json(allRestaurants)
+  })
+  .catch(next);
+});
+
+router.get('/activities', function (req, res, next) {
+  Activity.findAll()
+  .then(function (allActivities) {
+    res.json(allActivities)
+  })
+  .catch(next);
+});
+
+
+
+// days
+router.get('/days', function (req, res, next) {
+  Day.findAll()
+  .then(function (allDays) {
+    res.json(allDays)
+  })
+  .catch(next);
+});
+
+router.get('days/:id', function (req, res, next) {
+  Day.findById(req.params.id)
+  .then(function (day) {
+    res.json (day)
+  })
+  .catch (next);
+})
+
+
+router.post('/days/:id/restaurants', function (req, res, next) {
+  var specificDay = req.params.id;
+  Day_Restaurant.create({
+    dayId: specificDay,
+    restaurantId: req.body.id
+  })
+})
+
+
+router.put('/days/change', function (req, res, next) {
+  // update the day itinerary data
+});
+
+router.delete('/days/delete', function (req, res, next) {
+  // remove day.
+});
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,15 @@
 var Promise = require('bluebird');
-const router = require('express').Router();
-const Hotel = require('../models/hotel');
-const Activity = require('../models/activity');
-const Restaurant = require('../models/restaurant');
-// var router = require('express').Router();
-// var Hotel = require('../models/hotel');
-// var Restaurant = require('../models/restaurant');
-// var Activity = require('../models/activity');
+var router = require('express').Router();
+var Hotel = require('../models/hotel');
+var Restaurant = require('../models/restaurant');
+var Activity = require('../models/activity');
+
+var apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/', function(req, res, next) {
-  Promise.all([
-    Hotel.findAll(),
-    Restaurant.findAll(),
-    Activity.findAll()
-  ])
-  .spread(function(dbHotels, dbRestaurants, dbActivities) {
-    res.render('index', {
-      templateHotels: dbHotels,
-      templateRestaurants: dbRestaurants,
-      templateActivities: dbActivities
-    });
-  })
-  .catch(next);
+  res.render('index');
 });
 
-
-router.get('/api/hotels', function (req, res, next) {
-  Hotel.findAll()
-  .then(function (theHotels) {
-    res.json(theHotels)
-  })
-  .catch(next)
-});
-
-router.get('/api/restaurants', function (req, res, next) {
-  Restaurant.findAll()
-  .then(function (theRestaurants) {
-    res.json(theRestaurants)
-  })
-  .catch(next);
-});
-
-router.get('/api/activities', function (req, res, next) {
-  Activity.findAll()
-  .then(function (theActivities) {
-    res.json(theActivities)
-  })
-  .catch(next);
-});
-
-
-
-module.exports = router
+module.exports = router;
